3980a0dd1f57dd5787f0aef6236ad0735fd11de2
Saida Antonyan <saida.antonyan@mail.com>
2018-09-13 00:35:49 +0400

    commit for homework2

---
+++ homework2.js
@@ -0,0 +1,18 @@
+
+
+const a = "CS rocks";
+const b = 5;
+const c = a + b; 
+const d = [6, 25, 99]
+
+if(d[1]/b===b){
+console.log(a)} else {
+console.log("too bad")}
+
+if(d[0]+d[1]==="C++ is better than JV") {
+console.log("learn C++")}else {
+		console.log("R and Python are better")
+	}
+
+console.log(c)
+console.log(d[2]*b)
